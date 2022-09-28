19bcbb2c9727e887bffe8088b566d2020a0aed05
Harish-Athulya <harishath88@gmail.com>
2022-09-28 17:42:36 +0530

    Record Fetch with Image

--- lib/pages/details_get.dart
+++ lib/pages/details_get.dart
@@ -1,11 +1,16 @@
 import 'dart:io';
 
 import 'package:athulya_app/utils/constants.dart';
+import 'package:athulya_app/utils/firebase_file.dart';
+import 'package:athulya_app/utils/services_firebase.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:athulya_app/classes/food.dart';
+
+import 'image_page.dart';
 
 class DetailsGet extends StatefulWidget {
   final String branch;
@@ -22,9 +27,11 @@ class DetailsGet extends StatefulWidget {
 class _DetailsGetState extends State<DetailsGet> {
   @override
   Widget build(BuildContext context) {
+    // Map<String, dynamic> data = {"branch": 0};
     final documentId = Food.docId(widget.branch, widget.date, widget.food);
     return Scaffold(
       appBar: AppBar(
+        elevation: 0,
         backgroundColor: primaryColor,
         title: Text('Athulya Senior Care'),
       ),
@@ -45,20 +52,119 @@ class _DetailsGetState extends State<DetailsGet> {
               .get(),
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.done) {
-              Map<String, dynamic> data =
-                  snapshot.data!.data() as Map<String, dynamic>;
+              final data = snapshot.data!.data();
+              late final records;
+              // late Future<List<FirebaseFile>> futureFiles;
+              late Future<FirebaseFile> futureFiles;
+              if (data.toString() == 'null') {
+                return Text('No records found');
+              }
+              /* Map<String, dynamic> data =
+                  snapshot.data?.data() as Map<String, dynamic> ??
+                      {"Records": 0};
+ */
+              /* if (data['Records'] == 0) {
+                return Text('Null records');
+              } */
+
+              /* if (data.isEmpty) {
+                return Text("No records found for the current filter");
+              } */
               // Map<String, dynamic> data = snapshot.data as Map<String, dynamic>;
               // QueryDocumentSnapshot data =
               //     snapshot.data as QueryDocumentSnapshot;
               // return DocumentSnapshot(snapshot.data);
-              return Text(data['imageName']);
+              // return Text('${data.isEmpty} value');
+              // return Text(data['imageName']);
+              if (data != null) {
+                records = data;
+                futureFiles = ServicesFirebase.listFiles(
+                    // '${records['branch']}/${records['imageName']}');
+                    '${records['branch']}',
+                    '${records['imageName']}');
+/* 
+                futureFiles = ServicesFirebase.listAll(
+                    // '${records['branch']}/${records['imageName']}');
+                    '${records['branch']}');
+*/
+
+              }
+              // return Text(records['imageName']);
+              return FutureBuilder<FirebaseFile>(
+                  future: futureFiles,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      final files = snapshot.data;
+
+                      return Column(
+                        children: [
+/*                           ListView.builder(
+                              itemCount: files!.length,
+                              itemBuilder: ((context, index) {
+                                final file = files[index];
+                                return buildFile(context, file);
+                              })),
+*/
+                          ListTile(
+                            leading: ClipOval(
+                              child: Image.network(
+                                files!.url,
+                                width: 52,
+                                height: 52,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            title: Text(
+                              files.name,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: secondaryColor),
+                            ),
+                            onTap: () {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) =>
+                                      ImagePage(file: files)));
+                            },
+                          ),
+                          // Text('Athulya ${files.name}'),
+                          // Text('Athulya ${files?.ref}'),
+                          // Text('Athulya ${files?.url}')
+                        ],
+                      );
+                    } else if (snapshot.hasError) {
+                      return Text('Something went wrong');
+                    } else {
+                      return CircularProgressIndicator();
+                    }
+                  });
             } else if (snapshot.hasError) {
               return Text('Error occured ${snapshot.error}');
             } else if (snapshot.connectionState == ConnectionState.waiting) {
-              return Text("Loading");
+              return Center(child: CircularProgressIndicator());
             }
             return Text("Athulya");
           }),
+    );
+  }
+
+  Widget buildFile(BuildContext context, FirebaseFile file) {
+    return ListTile(
+      leading: ClipOval(
+        child: Image.network(
+          file.url,
+          width: 52,
+          height: 52,
+          fit: BoxFit.cover,
+        ),
+      ),
+      title: Text(
+        file.name,
+        style: TextStyle(fontWeight: FontWeight.bold, color: secondaryColor),
+      ),
+      onTap: () => Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => ImagePage(
+                file: file,
+              ))),
     );
   }
 }
--- lib/utils/services_firebase.dart
+++ lib/utils/services_firebase.dart
@@ -8,6 +8,7 @@ import 'firebase_file.dart';
 
 class ServicesFirebase {
   static Future<List<FirebaseFile>> listAll(String path) async {
+    print('${path} 123');
     final ref = FirebaseStorage.instance.ref(path);
     final result = await ref.listAll();
 
@@ -18,6 +19,8 @@ class ServicesFirebase {
         .map((key, value) {
           final ref = result.items[key];
           final name = ref.name;
+          print(name);
+
           final file = FirebaseFile(ref: ref, name: name, url: value);
 
           return MapEntry(key, file);
@@ -25,6 +28,42 @@ class ServicesFirebase {
         .values
         .toList();
     ;
+  }
+
+  // static Future<List<FirebaseFile>> listFiles(
+  static Future<FirebaseFile> listFiles(String branch, String fileName) async {
+    final ref = FirebaseStorage.instance.ref(branch);
+    final result = await ref.listAll();
+
+    // final urls = await _getDownloadLinks(result.items);
+
+    final urls = await ref.child(fileName).getDownloadURL();
+
+    print(urls);
+
+    final FirebaseFile ffile =
+        FirebaseFile(ref: ref, name: result.items.first.name, url: urls);
+
+    return ffile;
+
+/*     return urls
+        .asMap()
+        .map((index, url) {
+          final ref = result.items[index];
+          final name = ref.name;
+          // print(name);
+          // print(fileName);
+          final file = FirebaseFile(ref: ref, name: name, url: url);
+
+          if (name == fileName) {
+            print('${fileName}');
+          }
+          return MapEntry(index, file);
+        })
+        .values
+        .toList();
+    ;
+ */
   }
 
   static Future<List<String>> _getDownloadLinks(List<Reference> refs) =>
